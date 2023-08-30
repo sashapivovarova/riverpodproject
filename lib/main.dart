0f5252c05184f6fbc5e1d770fe2c02fa4e45b109
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-08-30 14:03:11 +0900

    appbar の左右にアイコンを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final helloWorldProvider = Provider((_) => 'Hello world');
-final myProvider = Provider((ref) {
-  return MyValue();
-});
-
-void MyValue() {}
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final String value = ref.watch(helloWorldProvider);
-
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Example')),
-        body: Center(
-          child: Text(value),
+      debugShowCheckedModeBanner: false,
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Appbar icon menu',
         ),
+        centerTitle: true,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            print('menu button is clicked');
+          },
+          icon: const Icon(
+            Icons.menu,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('search button is clicked');
+            },
+            icon: const Icon(
+              Icons.search_rounded,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              print('shopping button is clicked');
+            },
+            icon: const Icon(
+              Icons.shopping_cart_rounded,
+            ),
+          ),
+        ],
       ),
     );
   }
