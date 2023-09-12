29ba0dfc9d802be756047fd44cf0426ddd3c45f4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-12 11:44:20 +0900

    Message を chatpage に表示

--- lib/page/chat_page.dart
+++ lib/page/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpodproject/chatting/chat/message.dart';
 import 'package:riverpodproject/page/login_page.dart';
 
 class ChatPage extends StatefulWidget {
@@ -35,61 +36,38 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: const Text('Chat'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _authentication.signOut();
-              Navigator.pushReplacement(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation1, animation2) =>
-                      const ThirdPage(),
-                  transitionDuration: Duration.zero,
-                  reverseTransitionDuration: Duration.zero,
-                ),
-              );
-            },
-            icon: const Icon(
-              Icons.logout_rounded,
-            ),
-          ),
-        ],
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection(
-              '/chats/Zkbo6z9ZTaCUcMS7NpMJ/message',
-            )
-            .snapshots(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(8),
-                child: Text(
-                  docs[index]['text'],
-                  style: const TextStyle(
-                    fontSize: 20,
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: const Text('Chat'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _authentication.signOut();
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation1, animation2) =>
+                        const ThirdPage(),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero,
                   ),
-                ),
-              );
-            },
-            itemCount: docs.length,
-          );
-        },
-      ),
-    );
+                );
+              },
+              icon: const Icon(
+                Icons.logout_rounded,
+              ),
+            ),
+          ],
+        ),
+        body: Container(
+          child: const Column(
+            children: [
+              Expanded(
+                child: Messages(),
+              ),
+            ],
+          ),
+        ));
   }
 }
