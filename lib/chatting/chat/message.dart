a7052c659d266eb2f6f67b1a89fbd162357d47af
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-12 11:42:06 +0900

    message list を表示する機能を実装

---
+++ lib/chatting/chat/message.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        final chatDocs = snapshot.data!.docs;
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return Text(
+              chatDocs[index]['text'],
+            );
+          },
+          itemCount: chatDocs.length,
+        );
+      },
+    );
+  }
+}
