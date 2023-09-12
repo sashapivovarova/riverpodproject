f6a5e473e457ac19be91838846d15e830bf20f4d
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-12 14:41:36 +0900

    textfield に多い文字を入力しても表示される機能を実装

--- lib/chatting/chat/message.dart
+++ lib/chatting/chat/message.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:riverpodproject/chatting/chat/chat_bubble.dart';
 
 class Messages extends StatelessWidget {
