e7bd50f236bee12a17eacc028ce1de87c8c650a9
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-13 14:37:21 +0900

    api の response.body を表示

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:like_button/like_button.dart';
-import 'package:riverpodproject/model/model.dart';
+import 'package:riverpodproject/model/service.dart';
+import 'package:riverpodproject/model/user.dart';
 import 'loding_page.dart';
 import 'search_page.dart';
 import 'login_page.dart';
@@ -14,123 +15,24 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  static var nutrientName = [
-    'Vitamin A',
-    'Vitamin B',
-    'Vitamin C',
-    'Vitamin D',
-    'Calcium',
-    'Magnesium',
-    'Folate',
-    'Mineral',
-    'Iron',
-    'Probiotic'
-  ];
-
-  static var nutrientImage = [
-    'assets/1.png',
-    'assets/2.png',
-    'assets/3.png',
-    'assets/4.png',
-    'assets/5.png',
-    'assets/6.png',
-    'assets/7.png',
-    'assets/8.png',
-    'assets/9.png',
-    'assets/10.png',
-  ];
-
-  static var nutrientDescription = [
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-    'It\'s a nutrient we really need. Feel the difference in your body day by day!',
-  ];
-
-  final List<Nutrient> nutrientData = List.generate(
-    nutrientName.length,
-    (index) => Nutrient(
-      name: nutrientName[index],
-      image: nutrientImage[index],
-      description: nutrientDescription[index],
-    ),
-  );
-
   bool isLiked = false;
   int likeCount = 0;
+  List<User> _user = <User>[];
+  bool loading = false;
 
-  void showPopup(context, title, image, description) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: 450,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    child: Image.asset(
-                      image,
-                      width: 200,
-                      height: 200,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    title,
-                    style: const TextStyle(
-                        fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      description,
-                      maxLines: 3,
-                      style: const TextStyle(
-                        fontSize: 15,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  LikeButton(
-                    size: 30,
-                    isLiked: isLiked,
-                    likeCount: likeCount,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: const ButtonStyle(
-                        iconColor: MaterialStatePropertyAll(
-                      Colors.pink,
-                    )),
-                    child: const Text('Close'),
-                  )
-                ],
-              ),
-            ),
-          );
-        });
+  @override
+  void initState() {
+    super.initState();
+    Services.getInfo().then(
+      (value) {
+        setState(
+          () {
+            _user = value;
+            loading = true;
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -138,7 +40,7 @@ class _MyPageState extends State<MyPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: const Text('Home'),
+        title: Text(loading ? 'User' : 'loading...'),
         actions: [
           IconButton(
             onPressed: () {
@@ -191,45 +93,41 @@ class _MyPageState extends State<MyPage> {
         ],
       ),
       body: ListView.builder(
-        itemCount: nutrientData.length,
+        itemCount: _user.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              showPopup(
-                context,
-                nutrientName[index],
-                nutrientImage[index],
-                nutrientDescription[index],
-              );
-            },
-            child: Card(
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image.asset(
-                      nutrientImage[index],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(
-                      10,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(
-                          nutrientName[index],
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+          User user = _user[index];
+          return ListTile(
+            leading: const Icon(
+              Icons.account_circle_rounded,
+              color: Colors.grey,
+            ),
+            trailing: IconButton(
+              icon: const Icon(
+                Icons.smartphone_rounded,
+                color: Colors.red,
               ),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text(
+                          user.name,
+                        ),
+                        content: Column(
+                          children: [
+                            Text(
+                              user.phone,
+                            ),
+                          ],
+                        ),
+                      );
+                    });
+              },
+            ),
+            title: Text(user.name),
+            subtitle: Text(
+              user.email,
             ),
           );
         },
